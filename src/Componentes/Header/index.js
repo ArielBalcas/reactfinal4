e25694d5b9929c61233416b42818f59b7ee6979d
ArielBalcas <107375584+ArielBalcas@users.noreply.github.com>
2022-11-21 01:57:06 -0300

    first commit

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,26 @@
+
+import React from 'react'
+
+// import adidas  from "../../fotos/adidas.jpg"; 
+
+export const Header = () =>{
+return(
+       
+    <header>
+     <div className="menu">
+     <box-icon>name="menu" </box-icon>
+
+
+     </div>
+<a href="%">
+    <div className="logo">
+        <img src="./" alt="adidas"/>
+    </div>
+</a>
+
+
+    </header>
+
+)
+
+}
---
+++ src/Componentes/Productos/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+export const ProductosLista = () =>{
+return(
+         <div>
+
+    <h1>Productos</h1>
+        
+        </div>
+
+)
+
+}
